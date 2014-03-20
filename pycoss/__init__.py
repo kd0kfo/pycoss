cfc34555b5213607d0d712ad68cff2391e4ea9aa
David Coss <David.Coss@stjude.org>
2014-03-20 12:15:15 -0500

    Added CommandRunner.

--- pycoss/__init__.py
+++ pycoss/__init__.py
@@ -20,3 +20,36 @@ class HistoryConsole(code.InteractiveConsole):
 
     def save_history(self, histfile):
         readline.write_history_file(histfile)
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class CommandRunner(object):
+    def __init__(self, commands):
+        self.commands = commands
+
+    def run_command(self, command, args):
+        if not command:
+            return
+        if command not in self.commands:
+            raise ArgumentError("Unknown Command: %s" % command)
+
+    def help(self, command):
+        if not command in self.commands:
+            return None
+        return self.commands[command]
+
+    def list_commands(self):
+        if not self.commands:
+            return []
+        return sorted(self.commands.keys())
+
+    def argcount_assert(self, args, size, can_have_more=False):
+        errmsg = "Invalid number of arguments"
+        numargs = len(args)
+        if numargs < size:
+            raise ArgumentError(errmsg)
+        if not can_have_more and numargs != size:
+            raise ArgumentError(errmsg)
